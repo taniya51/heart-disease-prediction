b9cff35f596bc45077c6d710b16188ec5d3cd199
Taniya rathor <taniyarathor51@gmail.com>
2025-09-21 19:52:08 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ smodel = joblib.load("heart_model.pkl")
 st.title("❤ Heart Disease Prediction")
 
 age = st.number_input("Age", 20, 100, 50)
-sex = st.selectbox("Sex", ['Female','Male'])  
+sex = st.selectbox("Sex", ['Female','Male']) 
+sex = 0 if sex == 'Female' else 1 
 cp = st.selectbox("Chest Pain Type", [0, 1, 2, 3])
 trestbps = st.number_input("Resting Blood Pressure", 80, 200, 120) 
 chol = st.number_input("Cholesterol", 100, 600, 200) 
